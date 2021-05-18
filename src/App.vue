7732eec833faf31b6596749689491d03a2a8df13
0rv3 <11827620+Orve@users.noreply.github.com>
2021-05-19 03:29:49 +0900

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,47 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <myheader></myheader>
+    <p v-if="msg.length > 0">
+      {{msg}}
+    </p>
+    <p v-else>
+      no text
+    </p>
+    <input type="text" v-model="msg">
+    <button @click="clear()">clear</button>
+    {{ apitest }}
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import myheader from './components/myheader'
 
 export default {
-  name: 'App',
   components: {
-    HelloWorld
+    myheader
+  },
+  data () {
+    return {
+      msg: 'Hello World!',
+      apitest: ''
+    }
+  },
+  methods: {
+    clear () {
+      this.msg = ''
+    }
+  },
+  created () {
+    fetch('http://www.geonames.org/postalCodeLookupJSON?postalcode=10504&country=US')
+    .then( response => {
+      return response.json()
+    })
+    .then( json => {
+      this.apitest = json.postalcodes[0].adminName1
+    })
+    .catch( (err) => {
+      this.apitest = err // エラー処理
+    });
   }
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
